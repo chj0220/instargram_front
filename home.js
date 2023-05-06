632b56149a7fd669743d121eebbd6e0234bd6b23
jihong <clara5091@naver.com>
2023-05-07 06:05:01 +0900

    feat: 게시물 좋아요 구현

--- home.js
+++ home.js
@@ -79,12 +79,8 @@ function post(data){
     html+= imageHtml(data.postImages);
     html+=`     <div class="bottom_icons">
                     <div class="left_icons">
-                    <div class="heart_btn">
-                        <div
-                        class="sprite_heart_icon_outline"
-                        name="39"
-                        data-name="heartbeat"
-                        ></div>
+                    <div class="heart_btn" id="${data.postId}">
+                        ${fillHeart(data.postId, data.iliked)}
                     </div>
                     <div class="sprite_bubble_icon"></div>
                     </div>
@@ -92,7 +88,7 @@ function post(data){
 
                 <div class="likes m_text">
                     좋아요
-                    <span id="like-count-39">${data.likes}</span>
+                    <span id="like-count-${data.postId}">${data.likes}</span>
                     <span id="bookmark-count-39"></span>
                     개
                 </div>
@@ -103,7 +99,7 @@ function post(data){
                     </div>
                 </div>
 
-                <div class="timer">1시간 전</div>
+                <div class="timer">${elpasedTime(data.createdAt)}</div>
 
                 <div class="comment_field" id="add-comment-post37">
                     <input type="text" placeholder="댓글달기..." />
@@ -143,4 +139,95 @@ function getImages(images){
     var string='';
     images.forEach(image=>string+=`<div class="swiper-slide"><img src="${image}"></div>`);
     return string;
+}
+
+function elpasedTime(string){
+    const date = getDate(string);
+    const now = new Date(new Date().getTime()+3240 * 10000);
+
+    const betweenTime = Math.floor((now.getTime() - date.getTime()) / 1000 / 60);
+        if (betweenTime < 1) return '방금전';
+        if (betweenTime < 60) {
+            return `${betweenTime}분전`;
+        }
+
+        const betweenTimeHour = Math.floor(betweenTime / 60);
+        if (betweenTimeHour < 24) {
+            return `${betweenTimeHour}시간전`;
+        }
+
+        const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+        if (betweenTimeDay < 365) {
+            return `${betweenTimeDay}일전`;
+        }
+
+        return `${Math.floor(betweenTimeDay / 365)}년전`;
+}
+
+function getDate(string){
+    return new Date(string.replace(/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/, '$1-$2-$3 $4:$5:$6'));
+}
+
+function toggleLike(postId){
+    var fLike=document.getElementById('flike'+postId);
+    var like = document.getElementById('like'+postId);
+    var count = document.getElementById('like-count-'+postId);
+
+    if(fLike.style.display=="none"){
+        like.style.display="none";
+        fLike.style.display="block";
+        count.innerText=parseInt(count.textContent)-1;
+    }else{
+        like.style.display="block";
+        fLike.style.display="none";
+        count.innerText=parseInt(count.textContent)+1;
+    }
+    $.ajax({
+        method: "POST",
+        url: "http://52.78.86.193:8080/post/"+postId+"/like",
+        dataType: "json",
+        data: JSON.stringify(local_userid),
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader("Content-type","application/json");
+          xhr.setRequestHeader("Authorization", granttype + token);
+        },
+        success: function(data) {
+        },
+        error:function(request,status,error){   //데이터 주고받기가 실패했을 경우 실행할 결과
+          alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+        }
+      });
+}
+
+function fillHeart(postId, boolean){
+    if(boolean){
+        return `<div
+        class="sprite_heart_icon_outline"
+        id="flike${postId}"
+        data-name="heartbeat"
+        onclick="toggleLike(${postId})" 
+        style="display:none"
+        ></div>
+        <div
+        class="sprite_heart_icon_outline_on"
+        id="like${postId}"
+        data-name="heartbeat"
+        onclick="toggleLike(${postId})"
+        ></div>`;
+    }
+    else{
+        return `<div
+        class="sprite_heart_icon_outline"
+        id="flike${postId}"
+        data-name="heartbeat"
+        onclick="toggleLike(${postId})" 
+        ></div>
+        <div
+        class="sprite_heart_icon_outline_on"
+        id="like${postId}"
+        data-name="heartbeat"
+        style="display:none"
+        onclick="toggleLike(${postId})"
+        ></div>`;
+    }
 }
